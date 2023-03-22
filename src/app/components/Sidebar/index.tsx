e267e1a206abd0f6a7b1ed9d4c508b68698aeebb
Vincent Hou <vincenthou@VincentdeMacBook.local>
2023-03-23 07:59:40 +0800

    feat(help): 增加FAQ的公开飞书文档链接

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -2,25 +2,12 @@ import { FC, useContext } from 'react'
 import { Link } from '@tanstack/react-router'
 import cx from 'classnames'
 import { ConversationsContext } from '~app/context'
-import feedbackIcon from '~/assets/icons/feedback.svg'
-import settingIcon from '~/assets/icons/setting.svg'
+import { MdOutlineFeedback, MdHelp } from 'react-icons/md'
+import { AiFillSetting } from 'react-icons/ai'
 import logo from '~/assets/logo.svg'
 import logoDark from '~/assets/logo-dark.svg'
 import ConversationHistoryItem from './ConversationHistoryItem'
 import { ChatConversation } from '~types'
-
-function IconButton(props: { icon: string; active?: boolean }) {
-  return (
-    <div
-      className={cx(
-        'p-[3px] rounded-[4px] cursor-pointer hover:opacity-80',
-        props.active ? 'bg-[#5E95FC]' : 'bg-[#F2F2F2] bg-opacity-20',
-      )}
-    >
-      <img src={props.icon} className="w-5 h-5" />
-    </div>
-  )
-}
 
 const Sidebar: FC<{ chatId?: string }> = (props) => {
   const conversations = useContext(ConversationsContext)
@@ -33,14 +20,17 @@ const Sidebar: FC<{ chatId?: string }> = (props) => {
           <img src={logoDark} className="h-8 hidden dark:block" />
         </Link>
         <p className="text-white text-xs flex-1 ml-2">
-          本项目基于开源项目搭建，可以免费使用
+          基于开源项目<br></br>可以免费使用
         </p>
         <div className="flex flex-row mt-1 ml-3 gap-[10px]">
-          <a href="https://www.bilibili.com/video/BV1NM411p7CN/" target="_blank" rel="noreferrer" title="给我反馈">
-            <IconButton icon={feedbackIcon} />
+          <a href="https://www.bilibili.com/video/BV1NM411p7CN/" target="_blank" rel="noreferrer" >
+            <MdOutlineFeedback size="1.5em" color="#fff" title="给我反馈" />
+          </a>
+          <a href="https://seigy6zzam.feishu.cn/docx/KTp7dtDJZoSbbFxxmUpcUfXznhc" target="_blank" rel="noreferrer" >
+            <MdHelp size="1.5em" color="#fff" title="常见问题" />
           </a>
           <Link to="/setting">
-            <IconButton icon={settingIcon} />
+            <AiFillSetting size="1.5em" color="#fff" title="全局配置" />
           </Link>
         </div>
       </div>
